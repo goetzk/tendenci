eae2c0d9712138c1482341f76d23c59e1138cfc4
root <root@aeva-combined>
2020-02-27 07:41:46 +0000

    Add celery.py mostly following upstream docs
    This is copy paste, boilerplate has been updated and the file placed in
    celery_setup.py to remove the possibility of namespace clashes.

--- tendenci/__init__.py
+++ tendenci/__init__.py
@@ -1,2 +1,12 @@
 
 __version__ = "11.4.10"
+
+# https://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery_setup import celery_app
+
+# TODO: Unless its actually required, consider removing this entry
+__all__ = ('celery_app',)
+
+
---
+++ tendenci/celery_setup.py
@@ -0,0 +1,31 @@
+# Taken from
+# http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
+# as part of experimental porting
+
+# https://github.com/tendenci/tendenci/issues/836
+
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tendenci.settings')
+
+celery_app = Celery('tendenci')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django celery_app configs.
+celery_app.autodiscover_tasks()
+
+
+@celery_app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
